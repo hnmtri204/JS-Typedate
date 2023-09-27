45a9a34ccec33face27de1fec5a43ff94a5f1b60
hnmtri204 <120084560+hnmtri204@users.noreply.github.com>
2023-09-27 08:21:46 +0700

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+/*Thông tin sinh viên*/
+var Ten 
+var Masinhvien 
+var CCCD 
+var Tenlop 
+var tentruong 
+var điemtb 
+/*Thông tin công nhân*/
+var ten  
+var diachi 
+var sdt 
+var hesoluong
+/*Thông tin chiếc xe car*/
+var ten
+var hang
+var gia
+var mauson
